6446b6e77e567050fd7f50c78611bdd1c42ba950
yunuseyvz <yunus.yavuz@campus.lmu.de>
2024-08-17 23:12:49 +0200

    update page titlte

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ const fontSans = FontSans({
 export const metadata: Metadata = {
   metadataBase: new URL(DATA.url),
   title: {
-    default: DATA.name,
+    default: `Portfolio | ${DATA.name}`,
     template: `%s | ${DATA.name}`,
   },
   description: DATA.description,
