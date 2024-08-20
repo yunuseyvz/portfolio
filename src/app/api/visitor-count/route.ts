81a3c1edb70bb479b5314eea4775c945880da672
yunuseyvz <yunus.yavuz@campus.lmu.de>
2024-08-20 18:19:38 +0200

    add logging

--- src/app/api/visitor-count/route.ts
+++ src/app/api/visitor-count/route.ts
@@ -3,19 +3,25 @@ import { NextRequest, NextResponse } from 'next/server';
 import { kv } from '@vercel/kv';
 
 export async function GET(req: NextRequest) {
-    try {
-      // Get the current visitor count
-      let count: number = parseInt(await kv.get('visitors') || '0', 10);
-  
-      // Increment the visitor count
-      count = count + 1;
-  
-      // Store the updated count
-      await kv.set('visitors', count.toString());
-  
-      // Return the updated count
-      return NextResponse.json({ count });
-    } catch (error) {
-      return NextResponse.json({ error: 'Failed to update visitor count' }, { status: 500 });
-    }
+  try {
+    console.log('Received GET request for visitor count');
+
+    // Get the current visitor count
+    let count: number = parseInt(await kv.get('visitors') || '0', 10);
+    console.log('Current visitor count:', count);
+
+    // Increment the visitor count
+    count = count + 1;
+    console.log('Incremented visitor count:', count);
+
+    // Store the updated count
+    await kv.set('visitors', count.toString());
+    console.log('Updated visitor count stored:', count);
+
+    // Return the updated count
+    return NextResponse.json({ count });
+  } catch (error) {
+    console.error('Error updating visitor count:', error);
+    return NextResponse.json({ error: 'Failed to update visitor count' }, { status: 500 });
   }
+}
