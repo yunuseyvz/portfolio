31d3ea51868e544f0767e6f7b6a3ef5047a2ea3f
yunuseyvz <yunus.yavuz@campus.lmu.de>
2024-11-18 23:29:51 +0100

    create sitemap

---
+++ src/app/sitemap.tsx
@@ -0,0 +1,30 @@
+import type { MetadataRoute } from 'next'
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: 'https://yuemya.de/',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 1,
+        },
+        {
+            url: 'https://yuemya.de/projects',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.8,
+        },
+        {
+            url: 'https://yuemya.de/contact',
+            lastModified: new Date(),
+            changeFrequency: 'yearly',
+            priority: 0.5,
+        },
+        {
+            url: 'https://yuemya.de/blog',
+            lastModified: new Date(),
+            changeFrequency: 'monthly',
+            priority: 0.5,
+        },
+    ]
+}
