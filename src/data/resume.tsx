c5c9ecb215cbb833becc7ebaae541e28c009cccd
yunuseyvz <yunus.yavuz@campus.lmu.de>
2024-11-24 00:46:56 +0100

    add new projects

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -103,6 +103,56 @@ export const DATA = {
   ],
   projects: [
     {
+      title: "Next.js Business Website",
+      href: "https://www.dolphinmarket.de/",
+      dates: "2024",
+      active: true,
+      description:
+        "For my family's small business, I designed and developed a modern website using Next.js, Tailwind CSS and Shadcn. The website showcases business information, services, and contact details. Check it out!",
+      technologies: [
+        "TypeScript",
+        "Next.js",
+        "Vercel",
+        "Tailwind CSS",
+      ],
+      links: [
+        {
+          type: "View",
+          href: "https://www.dolphinmarket.de/",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/dolphin.png",
+      video: "",
+    },
+    {
+      title: "Saturnalia - A React Quiz Game",
+      href: "https://saturnalia.onrender.com/",
+      dates: "2024",
+      active: true,
+      description:
+        "For a game night with friends, I developed a quiz and buzzer game using React. The game is forked from a popular open-source project and customized to fit our needs. Check it out!",
+      technologies: [
+        "JavaScript",
+        "React",
+        "Gameboard.io",
+      ],
+      links: [
+        {
+          type: "Source",
+          href: "https://github.com/yunuseyvz/saturnalia",
+          icon: <Icons.github className="size-3" />,
+        },  
+        {
+          type: "Play",
+          href: "https://saturnalia.onrender.com/",
+          icon: <Icons.game className="size-3" />,
+        },  
+      ],
+      image: "/saturnalia.png",
+      video: "",
+    },
+    {
       title: "Bachelor Thesis Project",
       href: "https://github.com/yunuseyvz/Bachelorthesis_Phishing",
       dates: "2024",
@@ -131,47 +181,26 @@ export const DATA = {
         "",
     },
     {
-      title: "Prototype: Language Learning App",
-      href: "https://www.figma.com/proto/LskgvLIixXjYt6MTnyXXt2/AppPrototype?node-id=18-3&t=68zkrRk3OH4btXYn-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=18%3A3",
-      dates: "2021",
+      title: "Meme Generator",
+      dates: "2023",
       active: true,
       description:
-        "A team project focused on practicing product prototyping through a mobile app for language learning.",
+        "As part of a course project, we developed a meme generator web application. I focused on frontend development using React, while the backend was built with Express.js.",
       technologies: [
-        "Figma",
-        "Prototyping",
+        "JavaScript",
+        "React",
+        "Express.js",
       ],
       links: [
         {
           type: "Demo",
-          href: "https://www.figma.com/proto/LskgvLIixXjYt6MTnyXXt2/AppPrototype?node-id=18-3&t=68zkrRk3OH4btXYn-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=18%3A3",
-          icon: <Icons.figma className="size-3" />,
+          href: "",
+          icon: <Icons.globe className="size-3" />,
         },
       ],
-      image: "/walkietalkie.png",
-      video: "",
-    },
-    {
-      title: "Prototype: Car Pooling App",
-      href: "https://www.figma.com/proto/Yeni4yv3NOmEcVuXCtPC9H/WeCommuters?node-id=6-2&starting-point-node-id=6%3A2&t=KQ80qou9GuY6kNgt-1",
-      dates: "2021",
-      active: true,
-      description:
-        "Our team designed a prototype for a car pooling app. Through extensive user research and brainstorming sessions, we developed an interactive prototype using Figma.",
-      technologies: [
-        "Figma",
-        "Prototyping",
-        "User Research",
-      ],
-      links: [
-        {
-          type: "Demo",
-          href: "https://www.figma.com/proto/Yeni4yv3NOmEcVuXCtPC9H/WeCommuters?node-id=6-2&starting-point-node-id=6%3A2&t=KQ80qou9GuY6kNgt-1",
-          icon: <Icons.figma className="size-3" />,
-        },
-      ],
-      image: "/wecommuters.png",
-      video: "",
+      image: "/mememuc.png",
+      video:
+        "",
     },
     {
       title: "2D Sidescroller Minigame",
@@ -197,26 +226,47 @@ export const DATA = {
         "",
     },
     {
-      title: "Meme Generator",
-      dates: "2023",
+      title: "Prototype: Car Pooling App",
+      href: "https://www.figma.com/proto/Yeni4yv3NOmEcVuXCtPC9H/WeCommuters?node-id=6-2&starting-point-node-id=6%3A2&t=KQ80qou9GuY6kNgt-1",
+      dates: "2021",
       active: true,
       description:
-        "As part of a course project, we developed a meme generator web application. I focused on frontend development using React, while the backend was built with Express.js.",
+        "Our team designed a prototype for a car pooling app. Through extensive user research and brainstorming sessions, we developed an interactive prototype using Figma.",
       technologies: [
-        "JavaScript",
-        "React",
-        "Express.js",
+        "Figma",
+        "Prototyping",
+        "User Research",
       ],
       links: [
         {
           type: "Demo",
-          href: "",
-          icon: <Icons.globe className="size-3" />,
+          href: "https://www.figma.com/proto/Yeni4yv3NOmEcVuXCtPC9H/WeCommuters?node-id=6-2&starting-point-node-id=6%3A2&t=KQ80qou9GuY6kNgt-1",
+          icon: <Icons.figma className="size-3" />,
         },
       ],
-      image: "/mememuc.png",
-      video:
-        "",
+      image: "/wecommuters.png",
+      video: "",
+    },
+    {
+      title: "Prototype: Language Learning App",
+      href: "https://www.figma.com/proto/LskgvLIixXjYt6MTnyXXt2/AppPrototype?node-id=18-3&t=68zkrRk3OH4btXYn-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=18%3A3",
+      dates: "2021",
+      active: true,
+      description:
+        "A team project focused on practicing product prototyping through a mobile app for language learning.",
+      technologies: [
+        "Figma",
+        "Prototyping",
+      ],
+      links: [
+        {
+          type: "Demo",
+          href: "https://www.figma.com/proto/LskgvLIixXjYt6MTnyXXt2/AppPrototype?node-id=18-3&t=68zkrRk3OH4btXYn-1&scaling=scale-down&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=18%3A3",
+          icon: <Icons.figma className="size-3" />,
+        },
+      ],
+      image: "/walkietalkie.png",
+      video: "",
     },
     {
       title: "RoboRally - Digital Board Game",
