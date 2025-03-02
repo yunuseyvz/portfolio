0021615d5379570bb5ff9df6950ff62597b3f3c1
yunuseyvz <yunus.yavuz@campus.lmu.de>
2025-03-02 13:34:34 +0100

    upgrade dependencies and next

--- src/components/magicui/cool-mode.tsx
+++ src/components/magicui/cool-mode.tsx
@@ -236,5 +236,5 @@ export const CoolMode: React.FC<CoolModeProps> = ({ children, options }) => {
     }
   }, [options]);
 
-  return React.cloneElement(children as React.ReactElement, { ref });
+  return React.cloneElement(children as React.ReactElement<any>, { ref });
 };
