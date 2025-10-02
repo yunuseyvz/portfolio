ec574d59636f1db8f0b94cc52e61388336068de7
yunuseyvz <yunus.yavuz@campus.lmu.de>
2025-10-02 19:28:27 +0200

    fix rewrites

--- next.config.mjs
+++ next.config.mjs
@@ -12,6 +12,34 @@ const nextConfig = {
       },
     ],
   },
+  rewrites: async () => {
+    return [
+      {
+        source: "/game",
+        destination: "https://lastchicken.vercel.app/"
+      },
+      {
+        source: "/TemplateData/:path*",
+        destination: "https://lastchicken.vercel.app/TemplateData/:path*"
+      },
+      {
+        source: "/Build/:path*",
+        destination: "https://lastchicken.vercel.app/Build/:path*"
+      },
+      {
+        source: "/cv",
+        destination: "/api/generate-cv"
+      },
+      {
+        source: "/bachelorthesis",
+        destination: "https://supabase.yuemya.de/storage/v1/object/public/portfolio/projects/bachelorthesis-6l8tvuSS0YkNXorjmkbF8quiugjLpI.pdf"
+      },
+      {
+        source: "/certificate-of-excellence",
+        destination: "https://supabase.yuemya.de/storage/v1/object/public/portfolio/projects/excellence-HfSaGvfqqb1rhWqnc1BLXNmjxOZZW8.jpg"
+      }
+    ];
+  }
 };
 
 export default nextConfig;
