2e6f253eb8587c9061d9e309e79310bcb2f4c286
Yunus <yyavuz1999@gmail.com>
2026-06-01 17:00:00 +0200

    remove outdated pages; reduce bundle size

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,7 @@
 import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return clsx(inputs);
 }
 
 export function formatDate(date: string) {
